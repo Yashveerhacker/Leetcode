3bdf501d43de168ad4379aedff02d254367727d2
Yashveer Singh <y89769879@gmail.com>
2026-08-02 21:47:56 +0530

    Time: 0 ms (100%), Space: 7.7 MB (88.59%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==1)
+        {
+            return 1;
+        }
+        int p1 = 2;
+        int p2 = 1;
+
+        for(int i = 3; i <= n; i++)
+        {
+            int temp = p1+p2;
+            p2 = p1;
+            p1 = temp;
+        }
+        return p1;
+    }
+};
